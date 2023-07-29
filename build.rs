9ae099f04689b00efcd4f45ec73b098470c79c78
paddy <paddythepaddy@duck.com>
2023-07-29 20:57:26 +0800

    fix: Cargo build process should fail when vue build failed

--- build.rs
+++ build.rs
@@ -2,11 +2,12 @@ fn main() {
     println!("yt watcher build script started");
     //println!("cargo:rerun-if-changed=www/src");
     let pwd = std::env::current_dir().unwrap();
-    std::process::Command::new("npm.cmd")
+    assert!(std::process::Command::new("npm.cmd")
         .arg("run")
         .arg("build")
         .current_dir(pwd.join("www/"))
         .status()
-        .expect("Build vue project failed.");
+        .expect("Build vue project failed.")
+        .success());
     println!("yt-watcher build script finished");
 }
