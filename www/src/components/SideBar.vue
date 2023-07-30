e10269b09c5e5129679c06ebfb4e9fa7bb8fb34c
paddy <paddythepaddy@duck.com>
2023-07-30 14:30:13 +0800

    imp: Done porting to vue

---
+++ www/src/components/SideBar.vue
@@ -0,0 +1,280 @@
+<script setup lang="ts">
+import { computed, ref, type StyleValue, type ComputedRef } from 'vue'
+import * as utils from '../utils'
+const emit = defineEmits(['show_popup', 'update_video_list', 'set_sync_key'])
+
+const prop = defineProps({
+  yt_thumbnail: String,
+  yt_handle: String,
+  yt_display_name: String,
+
+  tw_thumbnail: String,
+  tw_handle: String,
+  tw_display_name: String,
+
+  sync_key: String,
+  sub_yt_channels: {
+    type: Array<string>,
+    required: true
+  },
+  sub_tw_channels: {
+    type: Array<string>,
+    required: true
+  }
+})
+const channel_input = ref('')
+const alarm_enabled = ref(false)
+const sync_key = ref(prop.sync_key)
+const sync_key_class: ComputedRef<StyleValue> = computed(() => {
+  return [sync_key.value != null && utils.verify_sync_key(sync_key.value) ? '' : 'error']
+})
+
+function copy_calendar_url() {
+  if (prop.sub_yt_channels.length == 0 && prop.sub_tw_channels.length == 0) {
+    console.log('No id for calendar to copy')
+    return
+  }
+  let url = utils.site_url + 'cal?'
+  if (prop.sub_yt_channels.length != 0) {
+    url += 'yt-ch=' + prop.sub_yt_channels.join(',')
+  }
+  if (prop.sub_tw_channels.length != 0) {
+    if (prop.sub_yt_channels.length != 0) {
+      url += '&'
+    }
+    url += 'tw-ch=' + prop.sub_tw_channels.join(',')
+  }
+  if (alarm_enabled.value) {
+    url += '&alarm=true'
+  }
+  navigator.clipboard.writeText(url).then(() => {
+    console.log('copy success')
+    emit('show_popup', 'Copied')
+  })
+}
+
+function copy_synced_calendar_url() {
+  if (prop.sync_key == null || prop.sync_key.length == 0 || !utils.verify_sync_key(prop.sync_key)) {
+    console.log('invalid sync key')
+    return
+  }
+  let url = utils.site_url + 'cal?key=' + prop.sync_key
+  if (alarm_enabled.value) {
+    url += '&alarm=true'
+  }
+  navigator.clipboard.writeText(url).then(() => {
+    console.log('copy success')
+    emit('show_popup', 'Copied')
+  })
+}
+
+function set_sync_key() {
+  if (sync_key.value != null && utils.verify_sync_key(sync_key.value)) {
+    utils.set_sync_key(sync_key.value)
+    emit('set_sync_key', sync_key.value)
+  }
+}
+
+function new_sync_key() {
+  utils.new_sync_key().then((key) => {
+    console.log(key)
+    if (key != null) {
+      sync_key.value = key
+      emit('set_sync_key', key)
+    }
+  })
+}
+
+function push_sync_key() {
+  if (sync_key.value != null && utils.verify_sync_key(sync_key.value)) {
+    utils.push_sync_key(sync_key.value, prop.sub_yt_channels, prop.sub_tw_channels)
+  }
+}
+</script>
+<template>
+  <div id="menu_header"></div>
+  <input type="text" placeholder="new channel url" v-model="channel_input" />
+  <br />
+  <button @click="$emit('clear_ch_preview')">Clear</button>
+  <button @click="$emit('import_list', channel_input)">Import list</button>
+  <br />
+  <button
+    :hidden="prop.yt_handle != undefined && prop.yt_handle.length != 0"
+    @click="$emit('load-youtube', channel_input)"
+  >
+    Check Youtube
+  </button>
+  <button
+    :hidden="prop.yt_handle == undefined || prop.yt_handle.length == 0"
+    @click="$emit('follow-youtube')"
+  >
+    Follow Youtube
+  </button>
+  <br />
+  <div id="yt_channel_preview" v-if="prop.yt_handle != null && prop.yt_handle.length != 0">
+    <img :src="prop.yt_thumbnail" class="channel_icon" />
+    <br />
+    <span class="channel_handle">{{ prop.yt_handle }}</span>
+    <br />
+    <span class="chanel_display_name">{{ prop.yt_display_name }}</span>
+  </div>
+  <button
+    :hidden="prop.tw_handle != undefined && prop.tw_handle.length != 0"
+    @click="$emit('load-twitch', channel_input)"
+  >
+    Check Twitch
+  </button>
+  <button
+    :hidden="prop.tw_handle == undefined || prop.tw_handle.length == 0"
+    @click="$emit('follow-twitch')"
+  >
+    Follow Twitch
+  </button>
+  <br />
+  <div id="tw_channel_preview" v-if="prop.tw_handle != null && prop.tw_handle.length != 0">
+    <img :src="prop.tw_thumbnail" class="channel_icon" />
+    <br />
+    <span class="channel_handle">{{ prop.tw_handle }}</span>
+    <br />
+    <span class="chanel_display_name">{{ prop.tw_display_name }}</span>
+  </div>
+  <br />
+  <div>
+    <input
+      type="text"
+      v-model="sync_key"
+      placeholder="synchronize key"
+      :class="sync_key_class"
+    /><br />
+    <button @click="set_sync_key">Set</button>
+    <button @click="new_sync_key">New</button>
+    <button @click="$emit('pull_sync_key')">Pull</button>
+    <button @click="push_sync_key">Push</button>
+  </div>
+  <br />
+  <div>
+    <button @click="copy_calendar_url">Copy calendar url</button>
+    <button @click="copy_synced_calendar_url">Copy synced calendar url</button>
+    <div style="margin-top: 5px; margin-bottom: 5px">
+      <input id="alarm" type="checkbox" v-model="alarm_enabled" hidden="true" />
+      <label for="alarm" :class="{ checked: alarm_enabled }">Alarm</label>
+    </div>
+  </div>
+  <hr />
+  <span>Tracking {{ prop.sub_yt_channels.length + prop.sub_tw_channels.length }} channels</span>
+  <ul>
+    <li v-for="(ch, idx) in prop.sub_yt_channels" v-bind:key="idx" class="channel_id">
+      <button class="del_btn" @click="$emit('unfollow_yt_ch', ch)">x</button>
+      {{ ch }}&nbsp;<a :href="'https://www.youtube.com/' + ch">
+        <img class="platform_icon" src="/youtube_32x32.png"
+      /></a>
+    </li>
+    <li v-for="(ch, idx) in prop.sub_tw_channels" v-bind:key="idx" class="channel_id">
+      <button class="del_btn" @click="$emit('unfollow_tw_ch', ch)">x</button>
+      {{ ch }}&nbsp;<a :href="'https://www.twitch.tv/' + ch">
+        <img class="platform_icon" src="/twitch_32x32.png"
+      /></a>
+    </li>
+  </ul>
+  <button @click="$emit('clear_all_channel')">Clear All</button>
+  <hr />
+  <p>
+    About:<br />
+    這是我為了自己看 vtuber 需求做的小工具<br />
+    在這個網頁可以查詢、追蹤想要的頻道，並且提供 iCal 連結讓其他行事曆 App 匯入<br />
+    本意是模仿 Holodex，但是可以用來追蹤訂閱數通常低於 Holodex 註冊門檻的小頻道<br />
+    因為這個網站是架在一個超爛雲端主機上，效能非常有限，如果想要追蹤訂閱數高於兩萬的 vtuber
+    頻道，還是推薦用功能比較完整的
+    <a href="https://holodex.net">Holodex</a><br />
+    <br />
+    This is a basic tool made for myself.<br />
+    It can track youtube channels ongoing/upcoming streams and provide a iCal resource to add to
+    calendar apps.<br />
+    This project is highly inspired by Holodex, but can be used to track small channels that doesn't
+    meet the register threshold of Holodex.<br />
+    This site is ran on the most basic cloud machine I can find, so if you want to track channels
+    that has over 20k subs,
+    <a href="https://holodex.net">Holodex</a> is still recommended.<br />
+    <br />
+    Contact: paddycup1 on discord <br />
+    Source:
+    <a href="https://github.com/PaddyThePaddy/yt-watcher"
+      >https://github.com/PaddyThePaddy/yt-watcher</a
+    ><br /><br />
+    <span style="font-size: 0.7em"
+      >Refresh and menu icon by <a href="https://icons8.com">Icons8</a></span
+    >
+  </p>
+  <div id="menu_footer"></div>
+</template>
+
+<style scoped>
+img.channel_icon {
+  border-radius: 50%;
+  max-width: 240px;
+}
+
+img.platform_icon {
+  height: 1.2em;
+  vertical-align: middle;
+}
+
+li.channel_id {
+  list-style-type: none;
+}
+
+button.del_btn {
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+input {
+  padding-left: 5px;
+  padding-right: 5px;
+  border: solid rgb(0, 101, 135) 2px;
+}
+
+input.error {
+  background-color: rgb(255, 111, 111);
+}
+
+label {
+  margin: 5px;
+  border: solid whitesmoke 1px;
+  padding-left: 5px;
+  padding-right: 5px;
+  user-select: none;
+}
+
+label:hover {
+  border: solid rgb(104, 217, 255) 1px;
+  color: #ffffff;
+}
+label.checked {
+  background-color: rgb(20, 139, 179);
+  border: solid rgb(20, 139, 179) 2px;
+}
+
+@media (pointer: fine) {
+  div#menu_footer {
+    height: 0px;
+    visibility: hidden;
+  }
+  div#menu_header {
+    height: 60px;
+    visibility: visible;
+  }
+}
+@media (pointer: none), (pointer: coarse) {
+  div#menu_footer {
+    height: 60px;
+    visibility: visible;
+  }
+  div#menu_header {
+    height: 0px;
+    visibility: hidden;
+  }
+}
+</style>
---
+++ www/src/utils.ts
@@ -0,0 +1,200 @@
+export let site_url = document.URL
+if (site_url.endsWith('index.html')) {
+  site_url = site_url.substring(0, site_url.length - 10)
+}
+if (!site_url.endsWith('/')) {
+  site_url += '/'
+}
+
+export type UpcomingEvent = {
+  start_date_time: string
+  start_timestamp_millis: number
+  thumbnail_url: string
+  title: string
+  description: string
+  target_url: string
+  ongoing: boolean
+  source: {
+    YoutubeChannel?: {
+      id: string
+      thumbnail_url: string
+      title: string
+      custom_url: string
+    }
+    TwitchChannel?: {
+      id: string
+      thumbnail_url: string
+      title: string
+      login: string
+    }
+  }
+  uid: string
+}
+
+export function get_yt_id_list(): string[] {
+  const cookies: string[] = document.cookie.split(';')
+  for (const c of cookies) {
+    const pair = c.split('=', 2)
+    if (pair[0].trim() == 'yt_id_list') {
+      return pair[1]
+        .trim()
+        .split(',')
+        .filter((s) => s.length != 0)
+    }
+  }
+  return []
+}
+
+export function set_yt_id_list(id_list: string[]) {
+  document.cookie = 'yt_id_list=' + id_list.join(',')
+}
+
+export function get_tw_id_list(): string[] {
+  const cookies: string[] = document.cookie.split(';')
+  for (const c of cookies) {
+    const pair = c.split('=', 2)
+    if (pair[0].trim() == 'tw_id_list') {
+      return pair[1]
+        .trim()
+        .split(',')
+        .filter((s) => s.length != 0)
+    }
+  }
+  return []
+}
+
+export function set_tw_id_list(id_list: string[]) {
+  document.cookie = 'tw_id_list=' + id_list.join(',')
+}
+
+export function get_sync_key() {
+  const cookies = document.cookie.split(';')
+  for (const c of cookies) {
+    const pair = c.split('=', 2)
+    if (pair[0].trim() == 'sync_key') {
+      return pair[1].trim()
+    }
+  }
+  return ''
+}
+
+export function set_sync_key(key: string) {
+  document.cookie = 'sync_key=' + key
+}
+
+export type ChannelInfo = {
+  display: string
+  handle: string
+  thumbnail: string
+}
+
+export function load_youtube_channel(query: string): Promise<ChannelInfo> {
+  return fetch(site_url + 'yt-ch?q=' + query)
+    .then((resp) => resp.json())
+    .then((resp: any) => {
+      return new Promise((resolve, reject) => {
+        if (resp.error == null) {
+          resolve({
+            display: resp.data.title,
+            handle: resp.data.custom_url,
+            thumbnail: resp.data.thumbnail
+          })
+        } else {
+          reject(resp.error)
+        }
+      })
+    })
+}
+
+export function load_twitch_channel(query: string): Promise<ChannelInfo> {
+  return fetch(site_url + 'tw-ch?q=' + query)
+    .then((resp) => resp.json())
+    .then((resp: any) => {
+      return new Promise((resolve, reject) => {
+        if (resp.error == null) {
+          resolve({
+            display: resp.data.title,
+            handle: resp.data.custom_url,
+            thumbnail: resp.data.thumbnail
+          })
+        } else {
+          reject(resp.error)
+        }
+      })
+    })
+}
+
+export function get_video_data(
+  yt_ch_list: string[],
+  tw_ch_list: string[]
+): Promise<UpcomingEvent[]> {
+  if (yt_ch_list.length == 0 && tw_ch_list.length == 0) {
+    return new Promise((resolve) => {
+      resolve([])
+    })
+  }
+  let url = site_url + 'data?'
+  if (yt_ch_list.length != 0) {
+    url += 'yt-ch=' + yt_ch_list.join(',')
+  }
+  if (tw_ch_list.length != 0) {
+    url += '&tw-ch=' + tw_ch_list.join(',')
+  }
+  return fetch(url)
+    .then((resp) => resp.json())
+    .then((resp) => {
+      return resp
+    })
+}
+
+export function verify_sync_key(key: string): boolean {
+  return /[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/.test(key)
+}
+
+export function new_sync_key(): Promise<string | undefined | void> {
+  return fetch(site_url + 'sync/new')
+    .then((resp) => resp.json())
+    .then((resp) => {
+      return resp.key
+    })
+}
+
+export function push_sync_key(sync_key: string, yt_ch: string[], tw_ch: string[]) {
+  if (!verify_sync_key(sync_key)) {
+    console.log('invalid sync key')
+    return
+  }
+  let url = site_url + 'sync/push?key=' + sync_key
+  if (yt_ch.length != 0) {
+    url += '&yt-ch=' + yt_ch.join(',')
+  }
+  if (tw_ch.length != 0) {
+    url += '&tw-ch=' + tw_ch.join(',')
+  }
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log(resp)
+    })
+}
+
+export function pull_sync_key(sync_key: string): Promise<{ yt_ch: string[]; tw_ch: string[] }> {
+  if (!verify_sync_key(sync_key)) {
+    console.log('invalid sync key')
+    return new Promise((_, reject) => reject())
+  }
+  return fetch(site_url + 'sync/pull?key=' + get_sync_key())
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((resp) => {
+      const ret = { yt_ch: [], tw_ch: [] }
+      if (resp.yt_ch != null) {
+        ret.yt_ch = ret.yt_ch.concat(resp.yt_ch)
+      }
+      if (resp.tw_ch != null) {
+        ret.tw_ch = ret.tw_ch.concat(resp.tw_ch)
+      }
+      return ret
+    })
+}
