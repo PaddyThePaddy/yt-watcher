968abb35f83b3f06e5e3d0e19d18d8a68e32e885
paddy <paddythepaddy@duck.com>
2023-07-30 18:20:58 +0800

    fix: Fixed vue build process failed in non windows os

--- build.rs
+++ build.rs
@@ -2,12 +2,22 @@ fn main() {
     println!("yt watcher build script started");
     //println!("cargo:rerun-if-changed=www/src");
     let pwd = std::env::current_dir().unwrap();
-    assert!(std::process::Command::new("npm.cmd")
-        .arg("run")
-        .arg("build")
-        .current_dir(pwd.join("www/"))
-        .status()
-        .expect("Build vue project failed.")
-        .success());
+    if std::env::consts::OS == "windows" {
+        assert!(std::process::Command::new("npm.cmd")
+            .arg("run")
+            .arg("build")
+            .current_dir(pwd.join("www/"))
+            .status()
+            .expect("Build vue project failed.")
+            .success());
+    } else {
+        assert!(std::process::Command::new("npm")
+            .arg("run")
+            .arg("build")
+            .current_dir(pwd.join("www/"))
+            .status()
+            .expect("Build vue project failed.")
+            .success());
+    }
     println!("yt-watcher build script finished");
 }
