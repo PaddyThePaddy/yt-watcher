460dd9848f204ce4450f895eb5547291130220e0
paddy <paddythepaddy@duck.com>
2023-08-02 11:33:12 +0800

    imp: cache necessary files

--- www/public/worker.js
+++ www/public/worker.js
@@ -3,14 +3,13 @@ var staticCacheName = 'stream-watcher'
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(['/'])
+      console.log('add cache')
+      return cache.addAll(['/', '/assets/index.css', '/assets/index.js'])
     })
   )
 })
 
 self.addEventListener('fetch', function (event) {
-  console.log(event.request.url)
-
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request)
